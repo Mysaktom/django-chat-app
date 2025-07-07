a34ea38b773ace2c6ea433c6d1f4f2fab392e056
Tomáš Trefný <mysaktom1@gmail.com>
2025-07-07 21:30:32 +0200

    Oprava chat.urls pro vyber mistnosti

--- chat/urls.py
+++ chat/urls.py
@@ -4,7 +4,15 @@ from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
-    path('chat/', views.chat_page, name='chat-page'),
+    # Stránka pro výběr místnosti (např. /chat/)
+    path('chat/', views.room_selection, name='room-selection'),
+    
+    # Stránka pro konkrétní chatovací místnost (např. /chat/obecny-pokec/)
+    path('chat/<str:room_name>/', views.chat_page, name='chat-page'),
+    
+    # Přihlašovací stránka (/)
     path('', LoginView.as_view(template_name='chat/login_page.html'), name='login'),
+    
+    # Odhlášení
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
