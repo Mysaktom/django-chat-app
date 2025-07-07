b4558528d0a536ce356ba9b8c4ad665d4b2776da
Tomáš Trefný <mysaktom1@gmail.com>
2025-07-07 19:43:24 +0200

    Finalni oprava inicializace v asgi.py

--- chat_projekt/asgi.py
+++ chat_projekt/asgi.py
@@ -2,16 +2,20 @@
 import os
 from django.core.asgi import get_asgi_application
 
-# NEJDŘÍVE nastavíme cestu k settings, ještě před ostatními importy z Djanga
+# Krok 1: Jako úplně první věc nastavíme cestu k settings.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_projekt.settings')
 
-# TEPRVE TEĎ, když Django ví, kde má nastavení, můžeme bezpečně importovat zbytek
+# Krok 2: Připravíme a "probudíme" Django pro HTTP provoz.
+# Tím se zajistí, že všechny aplikace a modely jsou správně načtené.
+django_asgi_app = get_asgi_application()
+
+# Krok 3: Teprve TEĎ, když je Django plně připravené, importujeme zbytek.
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,  # Použijeme už připravenou Django aplikaci
     "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
