4f491f1388b49ab1917658346023287018b579a0
Tomáš Trefný <mysaktom1@gmail.com>
2025-07-07 16:08:18 +0200

    Oprava pořadí inicializace v asgi.py

--- chat_projekt/asgi.py
+++ chat_projekt/asgi.py
@@ -1,11 +1,14 @@
 # chat_projekt/asgi.py
 import os
+from django.core.asgi import get_asgi_application
+
+# NEJDŘÍVE nastavíme cestu k settings, ještě před ostatními importy z Djanga
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_projekt.settings')
+
+# TEPRVE TEĎ, když Django ví, kde má nastavení, můžeme bezpečně importovat zbytek
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 import chat.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_projekt.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
