48712d788eb8a6003f6810a52da5610b5113f889
Tomáš Trefný <mysaktom1@gmail.com>
2025-07-07 21:35:07 +0200

    Doplnění chybějícího pohledu room_selection

--- chat/views.py
+++ chat/views.py
@@ -3,8 +3,16 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Message
 
+# TUTO FUNKCI PŘIDEJ:
 @login_required
-def chat_page(request):
-    # Načteme posledních 25 zpráv
+def room_selection(request):
+    return render(request, 'chat/room_selection.html')
+
+@login_required
+def chat_page(request, room_name): # <- Tuto funkci už tam máš, jen ji upravíme
+    # Zde bychom měli filtrovat zprávy podle room_name, ale to teď neřešme
     messages = Message.objects.all().order_by('-timestamp')[:25][::-1]
-    return render(request, 'chat/chat_page.html', {'messages': messages})
+    return render(request, 'chat/chat_page.html', {
+        'room_name': room_name,
+        'messages': messages
+    })
