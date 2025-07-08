05d1147f6058360a6357d5f7106eefabc83854d9
elefantCO <93212645+elefantCO@users.noreply.github.com>
2025-07-08 21:29:57 +0200

    auto vytvoreni mistnosti

--- chat/views.py
+++ chat/views.py
@@ -25,12 +25,7 @@ def room_selection(request):
 @login_required
 def chat_page(request, room_name):
     # Najdeme objekt místnosti, nebo vrátíme chybu 404, pokud neexistuje
-    try:
-        room = Room.objects.get(name=room_name)
-    except Room.DoesNotExist:
-        # Zde by se hodilo zobrazit hezčí chybovou stránku, ale pro teď stačí
-        from django.http import Http404
-        raise Http404("Místnost neexistuje")
+    room, created = Room.objects.get_or_create(name=room_name)
 
     messages = room.messages.all().order_by('timestamp')[:25]
     return render(request, 'chat/chat_page.html', {
